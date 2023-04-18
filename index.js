2778c23d6c93930a464ebc1bbe071d7e0810c0a2
ali taslimpoor <shayannniiii@gmail.com>
2023-04-18 13:52:31 +0300

    completed

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => renderBooks(data))
+  .catch(error => console.error('Error fetching books:', error));  
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = book.title;
     main.appendChild(h2);
   });
 }
